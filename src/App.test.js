7c4c89c350e622fc8026f392c949dc44968e879e
Saibou <saiboub80@gmail.com>
2023-05-31 11:53:26 -0400

    RR-test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// import renderer from 'react-test-renderer'
+
+// test('renders a snapshot', () => {
+//   const tree = renderer.create(<App/>).toJSON()
+//   expect(tree).toMatchSnapshot()
+// })
+
+test('renders text MickeyMouse', () => {
+  render(<App />)
+  const nameElement = screen.getByText(/mickey mouse/i)
+  expect(nameElement).toBeInTheDocument()
 });
